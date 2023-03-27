56bbe2f47e494c889fb19d279f8ee9aa9b88db7a
Dvoe4ka <dvoooe4ka@yandex.ru>
2023-03-27 11:17:06 +0300

    style: class_def  separated from previous line2

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,11 +3,11 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.*;
 
 import java.time.LocalDate;
+
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class User {
     @Setter
     int id;
