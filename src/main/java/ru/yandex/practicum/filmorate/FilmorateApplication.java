fa592c0c05a0ee439a95e315204fe1108bb7d3e2
Михаил <108143971+dv0e4ka@users.noreply.github.com>
2023-05-10 08:22:33 +0300

    style: 009

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -6,12 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FilmorateApplication {
     public static void main(String[] args) {
-
-
-        // TODO не дропать таблицу 04.27
-        // TODO найти хокей для поиска всех TODO
-
-
         SpringApplication.run(FilmorateApplication.class, args);
     }
 }
---
+++ src/test/java/ru/yandex/practicum/filmorate/dao/implement/FilmDaoImplTest.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.dao.implement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmDaoImplTest {
+
+}
