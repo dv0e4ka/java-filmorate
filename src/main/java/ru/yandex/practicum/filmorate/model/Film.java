1411a6de5d6a21d93328805b67fa7126f9935e3b
Михаил <108143971+dv0e4ka@users.noreply.github.com>
2023-05-11 04:44:44 +0300

    fix: delete unuse import

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.*;
-import org.springframework.data.annotation.Id;
 
 import javax.validation.constraints.*;
 import java.time.LocalDate;
