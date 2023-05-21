0ef5ecbff28951ed76a7a42055bfd5e6868fc3ad
Ershow <me@evershov.com>
2023-05-22 02:08:55 +0300

    Реализовать класс Director и его сервис, exception, контроллер, dao и dao implementation. Добавить Director в класс Film. Обновить FilmService с учетом добавления класса Director.
    Реализовать класс Director и его сервис, exception, контроллер, dao и dao implementation. Добавить Director в класс Film. Обновить FilmService с учетом добавления класса Director.

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/DirectorDao.java
@@ -0,0 +1,25 @@
+package ru.yandex.practicum.filmorate.dao;
+
+import ru.yandex.practicum.filmorate.model.Director;
+
+import java.util.List;
+
+public interface DirectorDao {
+    Director add(Director director);
+
+    List<Director> addDirectorFilm(long id, List<Director> directors);
+
+    List<Director> updateDirectorFilm(long id, List<Director> directors);
+
+    void deleteDirectorFilm(long id);
+
+    Director update(Director director);
+
+    void delete(int id);
+
+    List<Director> getAllDirectors();
+
+    List<Director> getAllDirectorsByFilm(long id);
+
+    Director getById(int id);
+}
--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.yandex.practicum.filmorate.exception.*;
 import ru.yandex.practicum.filmorate.model.ErrorResponse;
 
 @RestControllerAdvice
@@ -58,6 +57,13 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handle(GenreNotFoundException e) {
+        log.error(e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handle(DirectorNotFoundException e) {
         log.error(e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
