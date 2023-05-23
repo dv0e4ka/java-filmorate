b6f3fc2934f4fcb513ad93de6d62f2092cdf297e
Ekaterina <mirasolar@mail.ru>
2023-05-24 01:20:20 +0300

    коммит после rebase

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.model.ErrorResponse;
-import ru.yandex.practicum.filmorate.model.Film;
 
 @RestControllerAdvice
 @Slf4j
