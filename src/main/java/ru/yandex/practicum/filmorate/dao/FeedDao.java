99b36fe1c13fe903952e23142d79f0df859d4230
Aleks <aleks312t@gmail.com>
2023-05-30 04:26:27 +0300

    feat: добавлена модель Event

--- src/main/java/ru/yandex/practicum/filmorate/dao/FeedDao.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FeedDao.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.filmorate.dao;
 
+import ru.yandex.practicum.filmorate.model.Event;
+
 import java.util.List;
 
 public interface FeedDao {
     void addFeed();
 
-    List getFeed(int id);
+    List<Event> getFeed(long id);
 }
