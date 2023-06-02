7e6122b9bdf4cd656e6f3b5e440262afbd161472
Ershow <me@evershov.com>
2023-06-02 15:30:38 +0300

    Добавить ограничительные аннотации в класс Director.

--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -2,8 +2,15 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class Director {
+    @NotNull
     private Integer id;
+    @NotBlank
+    @NotEmpty
     private String name;
 }
